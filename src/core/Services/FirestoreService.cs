3b12a30d74d0b5835036f66103d2d431355ee1e9
Yuuki Wesp <ls-micro@ya.ru>
2020-08-02 05:49:30 +0300

    add firestore service

---
+++ src/core/Services/FirestoreService.cs
@@ -0,0 +1,22 @@
+﻿namespace spotify.services
+{
+    using Google.Cloud.Firestore;
+    using Microsoft.Extensions.Configuration;
+
+    public class FirestoreService
+    {
+        private FirestoreDb db { get; }
+
+        public FirestoreService(IConfiguration config) => 
+            db = FirestoreDb.Create(config["DB:ID"]);
+
+        public CollectionReference Cache => 
+            db.Collection("spotify")
+                .Document("data")
+                .Collection("cache");
+        public CollectionReference Settings => 
+            db.Collection("spotify")
+            .Document("data")
+            .Collection("settings");
+    }
+}
