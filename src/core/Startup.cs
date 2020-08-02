997b033f8a859bcb0afc3546a69cd528a6d30c50
Yuuki Wesp <ls-micro@ya.ru>
2020-08-02 05:50:10 +0300

    use firestore and spotify in startup

--- src/core/Startup.cs
+++ src/core/Startup.cs
@@ -4,11 +4,17 @@ namespace spotify.core
     using Microsoft.AspNetCore.Hosting;
     using Microsoft.Extensions.DependencyInjection;
     using Microsoft.Extensions.Hosting;
+    using Services;
 
     public class Startup
     {
-        public void ConfigureServices(IServiceCollection services) 
-            => services.AddControllers();
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddScoped<FirestoreService>();
+            services.AddSpotify();
+            services.AddControllers();
+        }
+
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment()) 
